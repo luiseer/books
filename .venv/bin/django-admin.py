24aebd2085046cd0b1516dbfa2bdac3b896eb7ca
luiseer <luiseer@gmail.com>
2022-05-23 15:54:16 -0500

    update boo model insert owner to User

--- .venv/bin/django-admin.py
+++ .venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/Users/luisestradarodriguez/Documents/GitHub/Academlo/Library/books/.venv/bin/python3
+#!/home/luis/Documents/git/Python/django_04/books/.venv/bin/python3
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
--- book/models.py
+++ book/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import uuid
+from core.models import User
 # Create your models here.
 
 class Book(models.Model):
@@ -8,6 +9,7 @@ class Book(models.Model):
     book_id = models.UUIDField(default=uuid.uuid4())
     genre = models.CharField(max_length=100)
     des = models.TextField(max_length=1000)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.title
