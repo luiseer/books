823d1e863a543e7c4f89ad2491e7495fa2f79266
luiseer <luiseer@gmail.com>
2022-05-23 16:04:38 -0500

    update model book

--- book/models.py
+++ book/models.py
@@ -9,7 +9,7 @@ class Book(models.Model):
     book_id = models.UUIDField(default=uuid.uuid4())
     genre = models.CharField(max_length=100)
     des = models.TextField(max_length=1000)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     
     def __str__(self):
         return self.title
