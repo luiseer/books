2e71b72f905cb1b8c5f87b74fc7f829ccc84b5c2
Mauricio Tamez <vitxlssreturn@gmail.com>
2022-05-27 08:04:29 -0500

    [FIX] Forgot to actually manage the permissions in the user view with the permission class UserPermission that we made.

--- core/views.py
+++ core/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 from .serializers import UserSerializer
 from .models import User
 from .permissions import UserPermission
+from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from  rest_framework import status
@@ -15,7 +16,15 @@ class UserViewSet(viewsets.ModelViewSet):
     
     serializer_class = UserSerializer
     queryset =  User.objects.all()
-    permission_classes = [UserPermission]
+
+    def get_permissions(self):
+        if self.action == 'create':
+            permission_classes = [AllowAny] 
+        elif self.action == 'list':
+            permission_classes = [IsAdminUser]
+        else:
+            permission_classes = [UserPermission]
+        return [permission() for permission in permission_classes]
     
     @action(detail=True, permission_classes=[UserPermission])
     def my_books(self, request, pk=None):
